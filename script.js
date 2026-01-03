7e5f258b40d594ff23176e9b340612b7776c3db0
Moise Shima <mitalimoses01@gmail.com>
2026-01-03 12:02:46 +0200

    LO3: Save form data to localStorage

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+function addField() { 
+  const input = document.createElement('input'); 
+  input.placeholder = 'Extra Field'; 
+  document.querySelector('form').appendChild(input); 
+}
+
+// LO3: Save form data to localStorage
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const name = document.querySelector("#name").value;
+    const email = document.querySelector("#email").value;
+
+    localStorage.setItem("user", JSON.stringify({ name, email }));
+    alert("Form data saved to localStorage!");
+});
 function addField() { const input = document.createElement('input'); input.placeholder = 'Extra Field'; document.querySelector('form').appendChild(input); }
